920491592c49fbbcc8499df46ab3d1ad327eca8e
Ajai <ajainarayanan@gmail.com>
2017-12-19 19:53:10 -0800

    Adds a generic popover to be re-used across CDAP

---
+++ cdap-ui/app/cdap/components/Popover/index.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import { Manager, Target, Popper, Arrow } from 'react-popper';
+require('./Popover.scss');
+
+export default class Popover extends Component {
+
+  propTypes = {
+    children: PropTypes.ReactElement,
+    target: PropTypes.ReactElement,
+    targetDimension: PropTypes.object.isRequired,
+    placement: PropTypes.oneOf([
+      'top',
+      'bottom',
+      'left',
+      'right',
+      'auto'
+    ])
+  };
+
+  state = {
+    showPopover: false
+  };
+
+  togglePopover = () => {
+    this.setState({showPopover: !this.state.showPopover});
+  };
+
+  showPopoverContent = () => {
+    if (this.state.showPopover) {
+      return (
+        <Popper placement={this.props.placement || 'auto'} className="popper">
+          {this.props.children}
+          <Arrow className="popper__arrow"/>
+        </Popper>
+      );
+    }
+    return null;
+  };
+
+  render() {
+    const TargetElement = this.props.target;
+    return (
+      <Manager>
+        <Target style={this.props.targetDimension} onClick={this.togglePopover}>
+          <TargetElement />
+        </Target>
+        {this.showPopoverContent()}
+      </Manager>
+    );
+  }
+}
